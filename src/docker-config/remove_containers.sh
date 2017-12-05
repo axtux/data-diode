ee50f59f1932014fea5ca7b9f85ba9b059546ecf
loic <loic.nyaki@gmail.com>
2017-12-05 08:03:28 +0100

    Network configuration + container configuration : ok.

---
+++ src/docker-config/remove_containers.sh
@@ -0,0 +1,5 @@
+# Stop and remove all containers
+sudo docker stop diode-in && sudo docker rm diode-in
+sudo docker stop diode-out && sudo docker rm diode-out
+sudo docker stop inside-machine && sudo docker rm inside-machine
+sudo docker stop outside-machine && sudo docker rm outside-machine
