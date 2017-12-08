384f74b7ca32bb0f8122e601925fd2515a6341ec
loic <loic.nyaki@gmail.com>
2017-12-08 12:19:07 +0100

    Commit d'après la présentation

--- src/start_containers.sh
+++ src/start_containers.sh
@@ -2,7 +2,9 @@
 # Building container : inner side of the diode
 cd internal/
 sudo docker build . -t diode_in-img
-sudo docker run -t -d --net intra-diode --ip 192.168.0.2  -p 8880:80 -p 9999:8888 --name diode-in --cap-add=NET_ADMIN --cap-add=NET_RAW diode_in-img
+#sudo docker run -t -d --net intra-diode --ip 192.168.0.2 --name diode-in --cap-add=NET_ADMIN --cap-add=NET_RAW diode_in-img
+sudo docker run -t -d --net secure-network --ip 10.0.0.2 -p 8880:80 --name diode-in --cap-add=NET_ADMIN --cap-add=NET_RAW diode_in-img
+#sudo docker run -t -d --net secure-network -p 10.0.0.2:8880:80 --name diode-in --cap-add=NET_ADMIN --cap-add=NET_RAW diode_in-img
 cd -
 
 # Building container : outter side of the diode
@@ -25,5 +27,6 @@ cd -
 
 
 # Creating connecting a part of the diode to the outside, and the other one to the inside
-sudo docker network connect --ip 10.0.0.2 secure-network diode-in
+#sudo docker network connect --ip 10.0.0.2  secure-network diode-in
+sudo docker network connect --ip 192.168.0.2  intra-diode diode-in
 sudo docker network connect --ip 200.0.0.2 external-network diode-out
