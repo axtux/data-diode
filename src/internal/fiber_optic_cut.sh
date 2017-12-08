b4178c8997e71fe94ab95bf159d0a8a24fa0a661
Axtux <axtux@hotmail.com>
2017-12-08 16:17:05 +0100

    cuts interface instead of IP

--- src/internal/fiber_optic_cut.sh
+++ src/internal/fiber_optic_cut.sh
@@ -2,4 +2,5 @@
 
 #Simulate cutting the fiber optic cable from the inside of the diode to the outside,
 #by dropping all packets originating from the secure network and going to the intra-diode network
-iptables -A OUTPUT -d 192.168.0.0/24 -j DROP
+#iptables -A OUTPUT -d 192.168.0.0/24 -j DROP
+iiptables -A OUTPUT -o eth0 -j DROP
